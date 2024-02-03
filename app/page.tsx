22ca9e8f11ec2feea8f0edbb53df898ef8f9f196
jbalikov <jbalikov@calpoly.edu>
2024-02-03 15:27:42 -0800

    Changed Vercel logo to Banjo Kazooie

--- app/page.tsx
+++ app/page.tsx
@@ -17,8 +17,8 @@ export default function Home() {
           >
             By{" "}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
+              src="/Banjo-Kazooie_logo_black.svg"
+              alt="Banjo Kazooie"
               className="dark:invert"
               width={100}
               height={24}
