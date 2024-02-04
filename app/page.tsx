d299e1ebc7a727442c3ccef7299e2247293860c9
Roman <roblack@calpoly.edu>
2024-02-04 12:10:56 -0800

    added search_bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,46 @@
+'use client'
+
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+
+  const tabs = ["1", "3", "5","6","7","11","12","18", "aaaaa","aabaa"]
+  const [tabSearch, setTabSearch] = useState<string[]>([])
+  const handleTabSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value == ''){
+      setTabSearch([])
+      return false
+    }
+    else{
+      setTabSearch(tabs.filter(w => w.includes(e.target.value)).slice(0,4))
+    }
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <div className="bg-gray-200 w-full h-12 flex items-center px-6">
-        <p className="mr-auto text-black text-2xl font-semibold">Search...</p>
+        <p className="mr-auto text-black text-2xl font-semibold">LOGO</p>
+        <p className="flex-grow text-center"> </p>
+        <form>
+          <div className="relative"> 
+            <input type="search" placeholder="Search" onChange={(e) => handleTabSearch(e)}/> 
+          </div>
+          {
+            (tabSearch.length > 0) &&
+              (<div className="absolute top-8 p-1 flex flex-col bg-slate-400">
+                {
+                  tabSearch.map(s => (<span>{s}</span>))
+                }
+              
+              </div>)
+          }
+        </form>
         <p className="flex-grow text-center"> </p>
         <p className="flex-grow text-center"> </p>
         <p className="flex-grow text-center text-black text-2xl font-semibold">Browse</p>
         <p className="ml-auto text-black text-2xl font-semibold">Log In</p>
       </div>
-      
-      {/* <div className="relative flex place-items-center before:absolute before:h-[200px] before:w-full sm:before:w-[500px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 lg:w-500 lg:h-200">
-          <code className="font-mono font-bold">Upload a Midi File</code>
-        </p>
-      </div>
-       */}
-      {/* <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30" style={{ width: '500px', height: '200px' }}>
-          <code className="font-mono font-bold">Upload a Midi File</code>
-        </p>
-      </div> */}
 
       <div className="relative flex place-items-center">
         <a
