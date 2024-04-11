e51195780686436a7d0de48ae5f7db9cfbc36d92
Roman <roblack@calpoly.edu>
2024-04-11 15:50:17 -0700

    added tab pdf downloading

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@
 import { useState } from "react";
 import Modal from "../components/Modal";
 import NavBar from "../components/NavBar";
+import PDFTab from "../components/PDFTab";
+import { PDFDownloadLink } from "@react-pdf/renderer";
 
 export default function Home() {
   const [tab, setTab] = useState<string | undefined>(undefined);
@@ -18,7 +20,6 @@ export default function Home() {
             className="text-xs mb-4 flex justify-center"
             style={{
               fontFamily: "Courier New, Courier, monospace",
-              width: "1000px",
             }}
           >
             <pre>
@@ -26,6 +27,22 @@ export default function Home() {
               {tab}
             </pre>
           </div>
+
+          <PDFDownloadLink
+            document={<PDFTab title={title!} tab={tab} />}
+            fileName={title}
+          >
+            {({ loading }) =>
+              loading ? (
+                "loading..."
+              ) : (
+                <button className="rounded-lg bg-gray-300 px-5 py-2 transition-colors hover:border-gray-200 hover:bg-gray-400 dark:border-neutral-500 dark:bg-gray-600">
+                  Download Tab as PDF
+                </button>
+              )
+            }
+          </PDFDownloadLink>
+          <div className="preserve-whitespace min-height:20px">{<br />} </div>
         </>
       ) : undefined}
       <Modal setTab={setTab} setTitle={setTitle} />
---
+++ components/PDFTab.tsx
@@ -0,0 +1,56 @@
+import {
+  Page,
+  Text,
+  View,
+  Document,
+  StyleSheet,
+  Font,
+} from "@react-pdf/renderer";
+
+Font.register({
+  family: "Roboto Mono",
+  src: "http://fonts.gstatic.com/s/robotomono/v4/hMqPNLsu_dywMa4C_DEpY50EAVxt0G0biEntp43Qt6E.ttf",
+});
+
+interface PDFProps {
+  title: string;
+  tab: string;
+}
+
+export default function PDFTab({ title, tab }: PDFProps): JSX.Element {
+  const styles = StyleSheet.create({
+    page: {
+      flexDirection: "row",
+      backgroundColor: "#E4E4E4",
+      paddingTop: 35,
+      paddingBottom: 65,
+      paddingHorizontal: 20,
+    },
+    section: {
+      margin: 10,
+      padding: 10,
+      flexGrow: 1,
+    },
+    title: {
+      fontSize: 24,
+      textAlign: "center",
+    },
+    tab: {
+      paddingTop: 10,
+      fontSize: 7,
+      fontFamily: "Roboto Mono",
+      textAlign: "center",
+    },
+  });
+
+  return (
+    <Document>
+      <Page size="A4" style={styles.page}>
+        <View style={styles.section}>
+          <Text style={styles.title}>{title}</Text>
+          <Text style={styles.tab}>{tab}</Text>
+        </View>
+      </Page>
+    </Document>
+  );
+}
