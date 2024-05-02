38d5897d5f03a965d94aad9f7dc447024d505329
Roman <roblack@calpoly.edu>
2024-05-02 11:18:45 -0700

    login/register page spacing updates

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,12 +5,11 @@ import NavBar from "../../components/NavBar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between dark:bg-slate-700">
+    <main className="flex flex-col justify-around dark:bg-slate-700 h-screen">
       <NavBar />
-      <LoginCard />
-      <div></div>
-      <div></div>
-      <div></div>
+      <div className="flex flex-col items-center justify-around w-screen">
+        <LoginCard />
+      </div>
     </main>
   );
 }
