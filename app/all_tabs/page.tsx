f5040f66cc1ca5df17bb3a9271016956394306e9
jbalikov <jbalikov@calpoly.edu>
2024-08-27 15:36:38 -0700

    Temp Print for local data

--- app/all_tabs/page.tsx
+++ app/all_tabs/page.tsx
@@ -40,7 +40,8 @@ export default function Home() {
     });
 
     const data = await response.json();
-    console.log(response.text);
+    console.log(response.text());
+    console.log(response.json());
     setAllTabs(data);
   };
 
