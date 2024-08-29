21ae9b740a6636eaea4eca0541ff733e7c85ee82
Roman <roblack@calpoly.edu>
2024-08-28 21:54:21 -0700

    register page auto routes to login

--- components/RegisterCard.tsx
+++ components/RegisterCard.tsx
@@ -2,8 +2,15 @@ import { Formik, Field, Form } from "formik";
 import Link from "next/link";
 import Image from "next/image";
 import "./LoginRegisterCard.css";
+import { useRouter } from "next/navigation";
 
 export default function RegisterCard() {
+  const router = useRouter();
+
+  const handleNavigation = (path: string) => {
+    router.push(path); // Navigate to the specified path
+  };
+
   return (
     <div className="group rounded-lg bg-gray-300 px-5 py-4 transition-colors flex flex-col justify-center items-center dark:bg-slate-600 relative">
       <h1 className="font-bold text-3xl mb-7 dark:bg-slate-600">Register</h1>
@@ -33,6 +40,11 @@ export default function RegisterCard() {
             body: loginFormData,
           });
           alert(await res.text());
+
+          if (res.status === 200) {
+            handleNavigation("/login");
+          }
+
           actions.setSubmitting(false);
         }}
       >
@@ -50,10 +62,7 @@ export default function RegisterCard() {
             name="password"
             placeholder="Password"
           />
-          <button
-            className="text-2xl font-bold"
-            type="submit"
-          >
+          <button className="text-2xl font-bold" type="submit">
             Submit
           </button>
           <Link
