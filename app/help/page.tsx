a1225e9e6e2a02ef9f4103848bcc9f2fedef393c
jbalikov <jbalikov@calpoly.edu>
2024-08-28 22:35:28 -0700

    Added support email

--- app/help/page.tsx
+++ app/help/page.tsx
@@ -32,6 +32,12 @@ export default function Home() {
           Have an audio file but not a midi file? Use this tool to convert the
           audio file: https://basicpitch.spotify.com/
         </div>
+        <br />
+        <div className="w-2/3">
+          Need specific help or info? Contact us at miditotabs@gmail.com
+          Have fun generating tabs!
+        </div>
+        <br />
         <Link
           className="rounded-lg bg-gray-300 hover:bg-gray-400 dark:bg-neutral-800/30 dark:hover:bg-gray-600 transition-colors px-3 py-2"
           href="/"
